88fe0ecbf5f7689bef8d7a79b9064e2c98469ca2
tanushka <tanushka1726@gmail.com>
2025-10-26 19:00:59 +0530

    edit link

--- backend/server.js
+++ backend/server.js
@@ -8,38 +8,38 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const sessionRoutes = require("./routes/sessionRoutes");
 const questionRoutes = require("./routes/questionRoutes");
-const {protect} = require("./middlewares/authMiddleware");
-const {generateInterviewQuestions , generateConceptExplanation} = require("./controllers/aiController");
-
+const { protect } = require("./middlewares/authMiddleware");
+const {
+  generateInterviewQuestions,
+  generateConceptExplanation,
+} = require("./controllers/aiController");
 
 const app = express();
 
 app.use(cookieParser());
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    methods:["GET","POST","PUT","DELETE"],
-    allowedHeaders:["Content-type","Authorization"],
+app.use(
+  cors({
+    origin: "https://interview-prep-ai-1-q9kt.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-type", "Authorization"],
     credentials: true,
-})
+  })
 );
 
 app.use(express.json());
 
-connectDB()
+connectDB();
 
-app.use("/api/auth",authRoutes);
-app.use("/api/sessions",sessionRoutes);
-app.use("/api/questions",questionRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/sessions", sessionRoutes);
+app.use("/api/questions", questionRoutes);
 
-app.use("/api/ai/generate-question" , protect , generateInterviewQuestions);
-app.use("/api/ai/generate-explanation", protect , generateConceptExplanation);
+app.use("/api/ai/generate-question", protect, generateInterviewQuestions);
+app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
-app.use("/uploads",express.static(path.join(__dirname,"uploads"),{}))
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=> console.log(`Server running on the port ${PORT}`));
-
-
- 
+app.listen(PORT, () => console.log(`Server running on the port ${PORT}`));
