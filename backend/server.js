3c1cd11b5b7095847947fa076a383a833142733a
HevenDev <hevendevofficial@gmail.com>
2025-10-31 02:01:30 +0530

    api

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
 
 const express = require("express");
+require("dotenv").config();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const path = require("path");
